ac4a3eff08ffe3eeda74e277fe5aecb5ceffe51f
Tyrn <alexey1911@gmail.com>
2025-01-02 21:01:08 +0300

    Navigation 1: router Stack filled, button style Link introduced

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,5 @@
 import { Text, View, StyleSheet } from "react-native";
+import { Link } from "expo-router";
 
 export default function Index() {
   return (
@@ -8,6 +9,9 @@ export default function Index() {
       <Text style={styles.text}>
         Home Screen
       </Text>
+      <Link href="/about" style={styles.button}>
+        Go to About screen
+      </Link>
     </View>
   );
 }
@@ -20,6 +24,11 @@ const styles = StyleSheet.create({
     justifyContent: 'center',
   },
   text: {
+    color: '#fff',
+  },
+  button: {
+    fontSize: 20,
+    textDecorationLine: 'underline',
     color: '#fff',
   },
 });
