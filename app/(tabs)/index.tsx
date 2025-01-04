94e50246546e57154dd9592e0bc301a93a4b154a
Tyrn <alexey1911@gmail.com>
2025-01-04 14:20:28 +0300

    (tabs)index.tsx: EmojiPicker connected

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -6,6 +6,7 @@ import Button from "@/components/Button";
 import ImageViewer from "@/components/Viewer";
 import IconButton from "@/components/IconButton";
 import CircleButton from "@/components/CircleButton";
+import EmojiPicker from "@/components/EmojiPicker";
 
 import PlaceholderImage from "@/assets/images/background-image.png";
 
@@ -13,6 +14,7 @@ export default function Index() {
   const [selectedImage, setSelectedImage] =
     useState<string | undefined>(undefined);
   const [showAppOptions, setShowAppOptions] = useState<boolean>(false);
+  const [isModalVisible, setIsModalVisible] = useState<boolean>(false);
 
   const pickImageAsync = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
@@ -35,7 +37,11 @@ export default function Index() {
   };
 
   const onAddSticker = () => {
-    // we will implement this later
+    setIsModalVisible(true);
+  };
+
+  const onModalClose = () => {
+    setIsModalVisible(false);
   };
 
   const onSaveImageAsync = async () => {
@@ -66,6 +72,9 @@ export default function Index() {
             onPress={() => setShowAppOptions(true)} />
         </View>
       )}
+      <EmojiPicker isVisible={isModalVisible} onClose={onModalClose}>
+        {/* A list of emoji component will go here */}
+      </EmojiPicker>
     </View>
   );
 }
